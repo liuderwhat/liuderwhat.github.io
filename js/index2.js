7efda16d58d6b7db3e8653a41c8136ad58bcf998
liuderwhat <33472364+liuderwhat@users.noreply.github.com>
2020-07-27 21:29:08 +0800

    upload
    upload 所有資料

---
+++ js/index2.js
@@ -0,0 +1,9 @@
+$(".btn-primary").click(function(){
+        $(".collapse").collapse('toggle');
+    });
+$(".btn-success").click(function(){
+    $(".collapse").collapse('show');
+});
+$(".btn-warning").click(function(){
+    $(".collapse").collapse('hide');
+});
